252340bba10b10c2bfeb4cb6b5d3735416cae6a3
Mahesh Yaddanapudi <zzzmahesh@gmail.com>
2021-06-08 17:50:12 +0530

    Tests with MongoTemplate

--- mongo-persistence/src/main/java/com/netflix/conductor/mongo/config/MongoConfiguration.java
+++ mongo-persistence/src/main/java/com/netflix/conductor/mongo/config/MongoConfiguration.java
@@ -21,6 +21,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.netflix.conductor.dao.ExecutionDAO;
@@ -32,6 +33,7 @@ import com.netflix.conductor.mongo.dao.MongoQueueDAO;
 
 @Configuration(proxyBeanMethods = false)
 @ConditionalOnProperty(name = "conductor.db.type", havingValue = "mongo")
+@EnableMongoRepositories(basePackages = {"com.netflix.conductor.mongo.repositories"})
 @Import({MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 public class MongoConfiguration {
 	
